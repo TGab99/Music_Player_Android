2ba7a96b9a6c8cf137c9bbb85d2e4808ae30a224
TGab99 <55789282+TGab99@users.noreply.github.com>
2021-09-22 16:10:20 +0200

    Add mediaPlayer, musicPause, musicPlay and musicStop functions

--- app/src/main/java/com/example/music_player/MainActivity.java
+++ app/src/main/java/com/example/music_player/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.music_player;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.sound);
+    }
+
+    public void musicPause(View view) {
+        mediaPlayer.pause();
+    }
+
+    public void musicPlay(View view) {
+        mediaPlayer.start();
+    }
+
+    public void musicStop(View view) {
+        mediaPlayer.stop();
+        mediaPlayer = MediaPlayer.create(this, R.raw.sound);
     }
 }
